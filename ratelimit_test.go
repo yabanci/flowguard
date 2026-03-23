647ac360a05112d9fca4b908dc4ea4c3564c35e5
Arsen Ozhetov <yabanciinbt@gmail.com>
2026-03-23 15:22:37 +0600

    refactor: extract Clock interface for testability

--- ratelimit_test.go
+++ ratelimit_test.go
@@ -2,38 +2,9 @@ package flowguard
 
 import (
 	"context"
-	"sync"
 	"testing"
 	"time"
 )
-
-// mockClock lets us control time in tests without real sleeps.
-type mockClock struct {
-	mu  sync.Mutex
-	now time.Time
-}
-
-func newMockClock(t time.Time) *mockClock {
-	return &mockClock{now: t}
-}
-
-func (c *mockClock) Now() time.Time {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.now
-}
-
-func (c *mockClock) Sleep(d time.Duration) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.now = c.now.Add(d)
-}
-
-func (c *mockClock) Advance(d time.Duration) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.now = c.now.Add(d)
-}
 
 func TestTokenBucket_Allow(t *testing.T) {
 	clk := newMockClock(time.Now())
